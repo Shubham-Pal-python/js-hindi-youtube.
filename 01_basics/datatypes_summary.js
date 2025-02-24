83f9361c29288723ae1481638b4988016a29fa45
Shubham-Pal-python <shubhampal7090@gmail.com>
2025-02-24 15:44:09 +0000

    SUMMARY

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,13 @@
+// primitive
+
+// 7 types : string, numbers, null, boolearn,undifined, symbol, bigint
+
+const score = 100
+const scorevalues = 100.3
+
+const isLOggedIn = false
+
+// reference (non primitive)
+
+// array, objects, functions
+
