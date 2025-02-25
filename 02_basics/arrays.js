7eb75b8c858b1cb797a09da51bd08cd6bf485077
Shubham-Pal-python <shubhampal7090@gmail.com>
2025-02-25 15:47:40 +0000

    array

---
+++ 02_basics/arrays.js
@@ -0,0 +1,30 @@
+// //arrays
+
+// const myArr =[0, 1, 2, 3, 4, 5]
+
+// // console.log(myArr[ 3]);
+
+// // array methods
+
+// // myArr.push(6)
+// // myArr.pop()
+
+// const newArr = myArr.join()
+
+// // console.log(myArr);
+// // console.log(typeof newArr);
+
+// // slice, spice
+
+// console.log("A", myArr);
+
+// const my1 = myArr.slice(1, 3)
+
+// console.log(my1);
+// console.log("b", myArr);
+
+
+// const my2 = myArr.splice(1, 3)
+// console.log("c", myArr);
+// console.log(my2);
+
