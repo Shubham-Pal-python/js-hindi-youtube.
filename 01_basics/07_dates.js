37297398850697cfef5b3ddc2176f0abc043e52a
Shubham-Pal-python <shubhampal7090@gmail.com>
2025-02-25 14:58:50 +0000

    dates

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -1,1 +1,20 @@
+// dates
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+let myCreateDate = new Date(2025, 0, 23)
+// console.log(myCreateDate.toDateString());
+
+let mtTymeStamp = Date.now()
+
+// console.log(mtTymeStamp);
+// console.log(myCreateDate.getTime());
+
+
+
+
+
 
